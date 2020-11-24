3ce06a999766a7cfdb029f3ada4966005ea93b22
forward469 <ypoymanov@gmail.com>
2020-11-24 13:36:34 +0300

    Fix bugs
    - import numpy in utils.py
    - Add Example in get_best_threshold

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,49 @@
+import numpy as np
+
+
 def get_best_threshold(y_true, scores, metric="precision"):
+    """
+    Parameters
+    ----------
+    y_true : numpy.ndarray
+        1d vector, true target values.
+    scores : numpy.ndarray
+        1d vector, estimator`s probability predictions, scores
+    metric : str
+        'precision': choose threshold based on precision
+        'recall': choose threshold based on recall
+        'f1_score': choose threshold based on harmonic average of
+                    precision and recall
+        'sensitivity': choose threshold based on sensitivity
+        'specificity': choose threshold based on specificity
+        'med_f1_score': choose threshold based on harmonic average of
+                    sensitivity and specificity
+    
+
+    Returns
+    -------
+    trials : dict
+        'threshold' : list of used scores as thresholds
+        'metrics' : list of metrics calculated with each
+                    threshold in trials['threshold']
+        'best_trial' : best threshold to use and it`s metric 
+        
+    
+    Example
+    -------
+    >>> from src.utils import get_best_threshold
+
+    >>> from sklearn.datasets import make_classification
+    >>> from sklearn.linear_model import LogisticRegression
+
+    >>> X, y = make_classification(n_samples=int(1e5))
+    >>> clf = LogisticRegression().fit(X,y)
+    >>> scores = clf.predict_proba(X)[:,1]
+    array([0.95702791, 0.27423805, 0.9905933 , ..., 0.02838837, 0.0339324 , 0.97921772])
+
+    >>> get_best_threshold(y, scores, metric="f1_score")["best_trial"] 
+    {'threshold': 0.4613591717894563, 'metric': 0.9146460033587697}
+    """
     y_true, scores = map(np.asarray, [y_true, scores])
 
     indices = scores.argsort()
@@ -35,6 +80,7 @@ def get_best_threshold(y_true, scores, metric="precision"):
         "f1_score": f1_score,
         "sensitivity": sensitivity,
         "specificity": specificity,
+        "med_f1_score": med_f1_score,
     }[metric]
 
     metric_vals = metric_func(tp, fp, tn, fn)
