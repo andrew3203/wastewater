9050a57b92da5d85fba545cbad892aed266d221f
forward469 <ypoymanov@gmail.com>
2020-11-24 13:09:26 +0300

    Change package name to "wastewater" in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='wastewater',
     packages=find_packages(),
     version='0.1.0',
     description='Применение машинного обучения для анализа сточных вод с помощью оптической спектроскопии',
